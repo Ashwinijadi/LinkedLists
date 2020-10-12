fec105010dbbc48840224116f38be690df292b90
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-12 09:37:52 +0530

    added: A simple LinkedList is created

--- MyNode.java
+++ MyNode.java
@@ -1,7 +1,28 @@
 package com.capgemini.linkedlist;
 
-public class MyNode {
-	public static void main(String[] args) {
-		System.out.println("Welcome to DataStructures");
+	public class MyNode<K>{
+		private K key;
+		private MyNode next;
+		
+		public MyNode(K key) {
+			this.key=key;
+			this.next=null;
+		}
+
+		public K getKey() {
+			return key;
+		}
+
+		public void setKey(K key) {
+			this.key = key;
+		}
+
+		public MyNode getNext() {
+			return next;
+		}
+
+		public void setNext(MyNode next) {
+			this.next = next;
+		}
 	}
-}
+
---
+++ MyNodeTest.java
@@ -0,0 +1,21 @@
+package com.capgemini.linkedlist;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyNodeTest {
+	@Test
+	public void givenThreeNumbersInLinkedList() {
+		MyNode<Integer>myFirstNode=new MyNode<>(56);
+		MyNode<Integer>mysecondNode=new MyNode<>(30);
+		MyNode<Integer>myThirdNode=new MyNode<>(70);
+		myFirstNode.setNext(mysecondNode);
+		mysecondNode.setNext(myThirdNode);
+		boolean result=myFirstNode.getNext().equals(mysecondNode)  && 
+				myFirstNode.getNext().getNext().equals(myThirdNode);
+		Assert.assertTrue(result);
+	}
+
+}
