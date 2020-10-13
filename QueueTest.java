64534f1563f6afefc05a605130996e53b591833e
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-13 10:29:41 +0530

    Modified: Functionality of removing elements in queue is added using List operations

--- QueueTest.java
+++ QueueTest.java
@@ -1,7 +1,5 @@
 package com.capgemini.linkedlist;
 
-import static org.junit.Assert.*;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -19,6 +17,24 @@ public class QueueTest {
 		queue.printQueue();
 		Assert.assertEquals((int) firstNode.getKey(), 70);
 
+	}
+
+	@Test
+	public void given3NumbersRemoveElementInQueue() {
+		MyNode<Integer> firstNode = new MyNode<Integer>(70);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(56);
+		Queue queue = new Queue();
+		queue.enqueue(firstNode);
+		queue.enqueue(secondNode);
+		queue.enqueue(thirdNode);
+		queue.printQueue();
+		INode remove = queue.dequeue();
+		queue.printQueue();
+		INode remove1 = queue.dequeue();
+		queue.printQueue();
+		INode remove2 = queue.dequeue();
+		Assert.assertEquals(thirdNode, remove2);
 	}
 
 }
