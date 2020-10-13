fe806303e6fb121fb36d8b02c0557c1edc37abde
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-13 10:15:53 +0530

    Modified: Functionality of removing elements in stack is added using List operations

--- StackTest.java
+++ StackTest.java
@@ -10,13 +10,34 @@ public class StackTest {
 		MyNode<Integer> firstNode = new MyNode<Integer>(56);
 		MyNode<Integer> secondNode = new MyNode<Integer>(30);
 		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
-		Stack stack= new Stack();
-	
+		Stack stack = new Stack();
+
 		stack.push(firstNode);
 		stack.push(secondNode);
 		stack.push(thirdNode);
 		stack.printStack();
-		Assert.assertEquals((int)firstNode.getKey(), 56);
+		Assert.assertEquals((int) firstNode.getKey(), 56);
 	}
-	
+
+	@Test
+	public void given3NumbersRemoveElementInStack() {
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+		Stack stack = new Stack();
+
+		stack.push(firstNode);
+		stack.push(secondNode);
+		stack.push(thirdNode);
+		stack.printStack();
+
+		INode removed = stack.pop();
+		stack.printStack();
+		INode removed1 = stack.pop();
+		stack.printStack();
+		INode removed2 = stack.pop();
+		INode top1 = stack.peak();
+		Assert.assertEquals(firstNode, removed2);
+		Assert.assertEquals(null, top1);
+	}
 }
